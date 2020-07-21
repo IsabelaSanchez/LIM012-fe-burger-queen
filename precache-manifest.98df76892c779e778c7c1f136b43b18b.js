e062df6cb431147343d6756fe0fb0d924aab069a
IsabelaSanchez <natyst_26@hotmail.com>
2020-07-20 20:40:49 -0500

    new again

---
+++ precache-manifest.98df76892c779e778c7c1f136b43b18b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acd967476728cb9dd116c4bfbd98d9e",
+    "url": "/LIM012-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "eb1e5546575a79b757c2",
+    "url": "/LIM012-fe-burger-queen/static/css/main.d2f1e9d5.chunk.css"
+  },
+  {
+    "revision": "3cae89246996b001141a",
+    "url": "/LIM012-fe-burger-queen/static/js/2.adf3900f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LIM012-fe-burger-queen/static/js/2.adf3900f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1e5546575a79b757c2",
+    "url": "/LIM012-fe-burger-queen/static/js/main.523a7288.chunk.js"
+  },
+  {
+    "revision": "75e6aef233fb23401d09",
+    "url": "/LIM012-fe-burger-queen/static/js/runtime-main.dd7744da.js"
+  },
+  {
+    "revision": "94473e1c247495e07ac2274b98b8d232",
+    "url": "/LIM012-fe-burger-queen/static/media/fondo-burger-queen.94473e1c.jpg"
+  },
+  {
+    "revision": "d792dd6ad70651c98b9e9328fc3e1fbe",
+    "url": "/LIM012-fe-burger-queen/static/media/title-burger-queen.d792dd6a.svg"
+  }
+]);
